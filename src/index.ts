a7712e1a5aae3897a83960636719da8ac83b13a0
mobileapps22 <mobileapps309@gmail.com>
2024-08-05 12:28:46 +0545

    Changes to be committed: 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
+app.use(express.json());
 app.get('/', (req, res) => {
           res.send('Hello World!');
 });
